9765a7d91281524ea02f9aad06970307401b00a6
neves350 <martimneves350@gmail.com>
2025-12-10 17:29:58 +0000

    add docs for api

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -13,28 +13,34 @@ import type { Filter, Todo } from 'src/common/interfaces/todo.model'
 import { TodosResponseDto } from './dto/todos-response.dto'
 import type { CreateTodoDto } from './dto/create-todo.dto'
 import type { UpdateTodoDto } from './dto/update-todo.dto'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('todos')
 @Controller('todos')
 export class TodosController {
 	constructor(private todosService: TodosService) {}
 
 	@Get()
+	@ApiOperation({ summary: 'Return a list of todos.' })
 	getAllTodos(@Query('filter') filter: Filter): TodosResponseDto {
 		const todos = this.todosService.findAllTodos(filter)
 		return new TodosResponseDto(todos)
 	}
 
 	@Get(':id')
+	@ApiOperation({ summary: 'Return a todo by id.' })
 	getTodoById(@Param('id') id: string): Todo {
 		return this.todosService.findTodoById(id)
 	}
 
 	@Post()
+	@ApiOperation({ summary: 'Create a new todo.' })
 	createTodo(@Body() createTodoDto: CreateTodoDto): Todo {
 		return this.todosService.newTodo(createTodoDto)
 	}
 
 	@Put(':id')
+	@ApiOperation({ summary: 'Update a todo.' })
 	updateTodo(
 		@Param('id') id: string,
 		@Body() updateTodoDto: UpdateTodoDto,
@@ -43,6 +49,7 @@ export class TodosController {
 	}
 
 	@Delete(':id')
+	@ApiOperation({ summary: 'Delete a todo.' })
 	deleteTodo(@Param('id') id: string): void {
 		return this.todosService.deleteTodo(id)
 	}
