c077f5eb3ecbd43db7985386ef84a82e10c01353
neves350 <martimneves350@gmail.com>
2025-12-10 16:02:17 +0000

    updated tests for update todo method

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -24,12 +24,22 @@ describe('TodosService', () => {
 		const result = service.findAllTodos('all')
 		expect(result).toHaveLength(3)
 	})
-	// it('should return all todos when filter = active', () => {
-	// 	const result = service.findAllTodos('active')
-	// 	expect(result).toHaveLength(2)
-	// })
-	// it('should return all todos when filter = completed', () => {
-	// 	const result = service.findAllTodos('completed')
-	// 	expect(result).toHaveLength(1)
-	// })
+	it('should return all todos when filter = active', () => {
+		const todo_1 = service.newTodo({ title: 'Todo 1' })
+		service.newTodo({ title: 'Todo 2' })
+		service.newTodo({ title: 'Todo 3' })
+		service.updateTodo(todo_1.id, { completed: true })
+
+		const result = service.findAllTodos('active')
+		expect(result).toHaveLength(2)
+	})
+	it('should return all todos when filter = completed', () => {
+		const todo_1 = service.newTodo({ title: 'Todo 1' })
+		service.newTodo({ title: 'Todo 2' })
+		service.newTodo({ title: 'Todo 3' })
+		service.updateTodo(todo_1.id, { completed: true })
+
+		const result = service.findAllTodos('completed')
+		expect(result).toHaveLength(1)
+	})
 })
